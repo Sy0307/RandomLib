1917961389f56a2a1b3abc32d387b79a54dd4d95
Sy03 <1370724210@qq.com>
2022-08-05 01:35:00 +0800

    ✨ feat(update standardout):

--- random.cpp
+++ random.cpp
@@ -1,58 +1,82 @@
 #include "random.h"
 
-string GetString::GetBalanceString(int len = 40,
-                                   int t = 30)
-{
-    int n = use.size();
-    random_device rd;
-    default_random_engine en((time(0)));
-    uniform_int_distribution<int> dis(0, n - 1);
-    string ans = "";
-    for (int i = 0; i < len; i++)
-    {
-        ans += use[dis(en)];
-    }
-    map<char, int> mp;
-    vector<int> analysis(n);
-    for (int i = 0; i < len; i++)
-    {
-        mp[ans[i]]++;
-    }
-    for (int i = 0; i < n; i++)
-    {
-        analysis[i] = mp[use[i]];
-    }
-    if (Variance(analysis) < t)
-    {
-        return GetBalanceString(len, t);
-    }
-    return ans;
-}
+using namespace std;
+// string GetString::GetBalanceString(int len = 40,
+//                                    int t = 30)
+// {
+//     int n = use.size();
+//     random_device rd;
+//     default_random_engine en((time(0)));
+//     uniform_int_distribution<int> dis(0, n - 1);
+//     string ans = "";
+//     for (int i = 0; i < len; i++)
+//     {
+//         ans += use[dis(en)];
+//     }
+//     map<char, int> mp;
+//     vector<int> analysis(n);
+//     for (int i = 0; i < len; i++)
+//     {
+//         mp[ans[i]]++;
+//     }
+//     for (int i = 0; i < n; i++)
+//     {
+//         analysis[i] = mp[use[i]];
+//     }
+//     if (Variance(analysis) < t)
+//     {
+//         return GetBalanceString(len, t);
+//     }
+//     return ans;
+// }
 
-Tp void GetString::print()
-{
-    for (auto i : use)
-    {
-        cout << i << " ";
-    }
-    cout << endl;
-}
-void GetString::print(string s)
-{
-    cout << s << endl;
-}
-void GetString::receive(vector<char> t)
-{
-    //    len = n;
-    //    use.resize(n);
-    GetString::use.clear();
-    for (auto &k : t)
-    {
-        GetString::use.push_back(k);
-    }
-}
+// Tp void GetString::print()
+// {
+//     for (auto i : use)
+//     {
+//         cout << i << " ";
+//     }
+//     cout << endl;
+// }
+// void GetString::print(string s)
+// {
+//     cout << s << endl;
+// }
+// void GetString::receive(vector<char> t)
+// {
+//     //    len = n;
+//     //    use.resize(n);
+//     GetString::use.clear();
+//     for (auto &k : t)
+//     {
+//         GetString::use.push_back(k);
+//     }
+// }
 
-vector<int> GetNumber::GetNumberList(int len, int l, int r, int &seed)
+// to fix bugs
+int GetNumber::GetSingleNumber(int l, int r, double per)
+{
+    if (l == -1)
+    {
+        l = rangelow;
+    }
+    if (r == -1)
+    {
+        r = rangehigh;
+    }
+    WinRandom rnd;
+    int ans = abs(rnd.randInt());
+
+    ans = ans % (r - l + 1) + l;
+    int percent = rnd.randUnsigned() % (10000 - 1) + 1;
+    if (percent < per * 10000)
+    {
+        return abs(ans);
+    }
+    return -abs(ans);
+    //   return sign==1?ans:abs(ans);
+}
+vector<int> GetNumber::GetNumberList(int len, int l, int r, double per) //per = 负数几率
 {
     if (len == -1)
         len = GetNumber::len;
@@ -65,17 +89,15 @@ vector<int> GetNumber::GetNumberList(int len, int l, int r, int &seed)
     {
         r = rangehigh;
     }
-
-    random_device rd;
-    default_random_engine en{seed};
-    uniform_int_distribution<int> dis(l + 1, r - 1);
     for (int i = 0; i < len; i++)
     {
-        ans[i] = dis(en);
+        ans[i] = GetSingleNumber(l, r, per);
     }
 
-    if (!check(ans))
-        return GetNumberList(len, l, r,seed);
+    // if (!check(ans))
+    //     return GetNumberList(len, l, r);
+
+    // not check variance now
     return ans;
 }
 Tp bool GetNumber::check(vector<T> &ans)
@@ -94,21 +116,6 @@ Tp bool GetNumber::check(vector<T> &ans)
         test.push_back(now.second);
     }
     return Variance(test) <= limit;
-}
-int GetNumber::GetSingleNumber(int l, int r,int len)
-{
-    if (l == -1)
-    {
-        l = rangelow;
-    }
-    if (r == -1)
-    {
-        r = rangehigh;
-    }
-    random_device rd;
-    default_random_engine en{rd()};
-    uniform_int_distribution<int> dis(l, r);
-    return dis(en);
 }
 
 double Variance(vector<int> &data)
@@ -144,21 +151,18 @@ void StringOutPut(string s)
     cout << "\"" << s << "\"" << endl;
 }
 
-vector<int> GetNumber::GetUniqueNumberList(int len, int l, int r,int& seed)
+vector<int> GetNumber::GetUniqueNumberList(int len, int l, int r)
 {
 
     vector<int> temp;
-    map<int,int> mp;
+    map<int, int> mp;
     mp.clear();
-    srand(seed);
     for (int i = 0; i < len; ++i)
     {
-        srand(seed);
-        int t = GetSingleNumber(l, r)+rand();
-        while (mp[t] != 0||t<l||t>r)
+        int t = GetSingleNumber(l, r) + rand();
+        while (mp[t] != 0 || t < l || t > r)
         {
-            srand(++seed);
-            t = GetSingleNumber(l, r)+rand();
+            t = GetSingleNumber(l, r) + rand();
         }
         mp[t]++;
         temp.push_back(t);
@@ -181,9 +185,9 @@ void System_Control::AddCount()
     freopen("DATA\\count.txt", "w", stdout);
     cout << n + 1 << endl;
     fclose(stdout);
- //   cout<<1<<endl;
-    auto path = "Data\\Data"+to_string(n+1)+".txt";
-  //  cout<<1<<endl;
+    //   cout<<1<<endl;
+    auto path = "Data\\Data" + to_string(n + 1) + ".txt";
+    //  cout<<1<<endl;
     freopen(path.c_str(), "w", stdout);
 }
 
--- random.h
+++ random.h
@@ -2,10 +2,59 @@
 //  C++ standard  = C++20
 //  Recommand compiler = g++
 
-#include <bits/stdc++.h>
+#include <vector>
+#include <assert.h>
+#include <windows.h>
+#include <wincrypt.h>
+#include <string>
+#include <map>
+#include <iostream>
+#include <random>
+#include <algorithm>
 #define Tp template <typename T>
 using ll = long long;
 using namespace std;
+
+class WinRandom
+{
+    HCRYPTPROV handle;
+
+public:
+    WinRandom()
+    {
+        handle = NULL;
+        CryptAcquireContext(
+            (HCRYPTPROV *)&handle, NULL, NULL,
+            PROV_RSA_FULL, 0);
+    }
+    ~WinRandom()
+    {
+        if (handle != NULL)
+            CryptReleaseContext(handle, 0);
+    }
+    bool randBuf(void *dest, int len)
+    {
+        if (handle == NULL)
+            return false;
+        return CryptGenRandom(handle, len, (BYTE *)dest);
+    }
+#define _(func, typ)                                \
+    typ func()                                      \
+    {                                               \
+        typ ret = 0;                                \
+        assert(randBuf((void *)&ret, sizeof(ret))); \
+        return ret;                                 \
+    }
+    _(randInt, int)
+    _(randLong, long long)
+    _(randUnsigned, unsigned)
+    _(randUnsignedLong, unsigned long long)
+    _(randShort, short)
+    _(randUnsignedShort, unsigned short)
+    _(randChar, char)
+    _(randUnsignedChar, unsigned char)
+    _(randSignedChar, signed char)
+};
 
 class GetString
 {
@@ -26,11 +75,11 @@ private:
 class GetNumber
 {
 public:
-    vector<int> GetNumberList(int len, int l, int r, int &seed);
+    vector<int> GetNumberList(int len, int l, int r, double per = 1);
     Tp bool check(vector<T> &ans);
-    int GetSingleNumber(int l = -1, int r = -1,int len = 0);
-    vector<int> GetUniqueNumberList(int len, int l, int r, int &seed);
-    GetNumber(int a, int b,int c = 0)
+    int GetSingleNumber(int l = -1, int r = -1, double sign = 1.0);
+    vector<int> GetUniqueNumberList(int len, int l, int r);
+    GetNumber(int a, int b, int c = 0)
     {
         rangehigh = b;
         rangelow = a;
@@ -48,10 +97,8 @@ public:
     void AddCount();
 };
 
-
-
-//extra content
-// Tp void StandardPrint(vector<T>);
+// extra content
+//  Tp void StandardPrint(vector<T>);
 
 double Variance(vector<int> &data);
 
@@ -67,7 +114,7 @@ Tp vector<T> Input(int length)
     return ans;
 }
 
-Tp void StandardPrint(vector<T> &data)
+Tp void StandardPrint(vector<T> &data , bool f = 1)
 {
     //标准LC Json输入
     cout << "[";
@@ -79,8 +126,9 @@ Tp void StandardPrint(vector<T> &data)
         else
             cout << d;
     }
-    cout << "]" << endl;
+    cout << "]" ;
+    if(f)
+        cout << endl;
 }
 
 void StringOutPut(string s);
-
